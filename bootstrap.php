64b9d6cdfbcdb8cdc7c98d4c173b9aaf7df56b80
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-11-10 15:12:41 +0100

    Bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,14 @@
 <?php
 
+use Dotenv\Dotenv;
+use Src\System\DatabaseConnector;
+
+
+require "vendor/autoload.php";
+
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
+
+
+$dbConnection = (new DatabaseConnector())->getConnection();
+
